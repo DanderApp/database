599f4eaecb346f029c91005eebd0d3b9c817c57a
dlinch <dlinch33@gmail.com>
2016-01-11 11:10:16 -0700

    added link to readme for lucidchart data model, added new migration for simpler database

--- migrations/20160109135901_initial.js
+++ migrations/20160109135901_initial.js
@@ -14,11 +14,13 @@ exports.up = function(knex, Promise) {
       return Promise.all([
       knex.schema.createTable('dog', function(table){
         table.increments('dog_id').primary().unsigned();
+        table.integer('petfinder_id');
         table.string('name');
         table.string('age');
         table.string('sex');
         table.integer('breed_id').unsigned().references('breed_id').inTable('breed').onDelete('cascade');
         table.string('pic_url');
+        table.boolean('is_adopted');
         table.integer('shelter_id').unsigned().references('shelter_id').inTable('shelter').onDelete('cascade');
       }),
       knex.schema.createTable('person', function(table){
---
+++ migrations/20160111103304_simpleConnection.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('user', function(table){
+    table.increments().primary().unsigned();
+    table.string('name');
+    table.string('email')
+    table.string('profile_id');
+
+  }).then(function(){
+    return knex.schema.createTable('connection', function(table){
+      table.increments().primary().unsigned();
+      table.integer('user_id').unsigned().references('id').inTable('user').onDelete('cascade');
+      table.integer('petfinder_id');
+    })
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('connection').then(function(){
+    return knex.schema.dropTable('user');
+  })
+};
