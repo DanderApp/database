f156c84347d20897074a47e7e4a3534e41092ae3
dlinch <dlinch33@gmail.com>
2016-01-13 10:24:02 -0700

    added zipcode to users table. last time I edit the migration file I swear

--- migrations/20160111103304_simpleConnection.js
+++ migrations/20160111103304_simpleConnection.js
@@ -5,6 +5,7 @@ exports.up = function(knex, Promise) {
     table.string('name');
     table.string('email')
     table.string('profile_id');
+    table.integer('zipcode')
 
   }).then(function(){
     return knex.schema.createTable('connection', function(table){
